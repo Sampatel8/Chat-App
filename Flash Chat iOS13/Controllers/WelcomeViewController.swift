da33427c7741b8e6068eded53f1370311c951257
Smit Patel <34744262+Sampatel8@users.noreply.github.com>
2021-12-09 02:02:41 -0500

    Initial Commit to Main

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = K.appName
+        
+        for latter in titleText{
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(latter)
+            }
+            charIndex += 1
+        }
+        
+        
+
+       
+    }
+    
+
+}
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Smit Patel on 2021-07-29.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message{
+    let sender : String
+    let body : String
+    
+}
